1007d49ef1b0038bef63b8cb30dcb1e8bba55693
NotAnNPC <allmatty@gmail.com>
2020-05-20 11:52:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 //retrieve all the command files from the Commands folder
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-//take name of
+
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
+  // set a new item in the Collection
+	// with the key as the command name and the value as the exported module
   client.commands.set(command.name, command);
 }
 
